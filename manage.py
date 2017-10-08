20e8e941756c3ca41da0643cc01b13b2398de777
d.vitvitskiy <d.vitvitskiy@smartweb.com.ua>
2017-10-08 18:26:48 +0300

    fixed docker mistakes

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return 'Fitness Tracker!'
+    return 'Fitness Tracker! !!!'
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True)
+    app.run(host="0.0.0.0", debug=True)
