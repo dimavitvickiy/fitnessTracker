ba66cb7c4d07f02809cbc1700149d1ea49bf24c3
d.vitvitskiy <d.vitvitskiy@smartweb.com.ua>
2017-10-26 14:49:49 +0300

    small fixes

--- frontend_static/js/Application/reducers/schedule.js
+++ frontend_static/js/Application/reducers/schedule.js
@@ -4,10 +4,9 @@ export const CHANGE_INPUT = 'CHANGE_INPUT';
 const schedule = (state = {}, action) => {
     switch (action.type) {
         case CHANGE_INPUT:
-            return {
-                ...state,
-                text: action.text,
-            };
+            return Object.assign({}, state, {
+                text: action.text
+            });
         default:
             return state
     }
