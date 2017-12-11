5370865d454510d1780fe6e85dc84721a512951d
d.vitvitskiy <d.vitvitskiy@smartweb.com.ua>
2017-12-11 02:22:45 +0200

    add mutation tests

--- app/helpers/password/__init__.py
+++ app/helpers/password/__init__.py
@@ -5,3 +5,15 @@ def generate_password(password):
     pswd = password.encode('utf-8')
     pswd_hash = hashlib.sha256(pswd)
     return pswd_hash.hexdigest()
+
+
+def generate_password_first_mutation(password):
+    pswd = password.encode('utf-8')
+    pswd_hash = hashlib.sha256(pswd)
+    return pswd_hash.digest()
+
+
+def generate_password_second_mutation(password):
+    pswd = password.encode('utf-8')
+    pswd_hash = hashlib.md5(pswd)
+    return pswd_hash.hexdigest()
--- tests/helpers/password_test.py
+++ tests/helpers/password_test.py
@@ -1,6 +1,24 @@
-from app.helpers.password import generate_password
+import pytest
+
+from app.helpers.password import (
+    generate_password,
+    generate_password_first_mutation,
+    generate_password_second_mutation,
+)
 
 
 def test_generate_password():
     correct_hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
     assert correct_hash == generate_password('password')
+
+
+@pytest.mark.xfail
+def test_generate_password_first_mutation():
+    correct_hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+    assert correct_hash == generate_password_first_mutation('password')
+
+
+@pytest.mark.xfail
+def test_generate_password_second_mutation():
+    correct_hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+    assert correct_hash == generate_password_second_mutation('password')
